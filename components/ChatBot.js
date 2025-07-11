f2d3bf420eb6f73291ee1fe24858e4b4956b8c08
rob-cost <robertocostantino3@gmail.com>
2025-07-11 09:06:42 +0200

    Created a chat-bot
    Tried to implement a chat-bot assistant (work in progress)

--- components/ChatBot.js
+++ components/ChatBot.js
@@ -1,31 +1,67 @@
 import { useEffect, useState } from "react";
 import { GiftedChat } from "react-native-gifted-chat";
 import { View, StyleSheet } from "react-native";
+/* import { Dialogflow_V2 } from "react-native-dialogflow"; */
+
+/* import { dialogflowConfig } from "../env"; */
+
+const BOT_USER = {
+  _id: 2,
+  name: "Chat-Bot",
+  avatar: "https://i.imgur.com/7k12EPD.png",
+};
 
 const ChatBot = () => {
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
+    Dialogflow_V2.setConfiguration(
+      dialogflowConfig.client_email,
+      dialogflowConfig.private_key,
+      Dialogflow_V2.LANG_ENGLISH_US,
+      dialogflowConfig.project_id
+    );
+
     setMessages([
       {
         _id: 1,
         text: "Hi! I am your chatbot. How may I help you with today?",
         createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: "Chat-Bot",
-          avatar: "https://i.imgur.com/7k12EPD.png",
-        },
+        user: BOT_USER,
       },
     ]);
   }, []);
 
-  // sending message function
+  /*   const handleGoogleResponse = (result) => {
+    let text = result.queryResult.fulfillmentMessages[0].text.text[0];
+    sendBotResponse(text);
+  };
+
   const onSend = (newMessages) => {
     setMessages((previousMessages) =>
       GiftedChat.append(previousMessages, newMessages)
     );
+    let message = messages[0].text;
+    Dialogflow_V2.requestQuery(
+      message,
+      (result) => handleGoogleResponse(result),
+      (error) => console.log(error)
+    );
   };
+
+  const sendBotResponse = (text) => {
+    let msg = {
+      _id: this.state.messages.length + 1,
+      text,
+      createdAt: new Date(),
+      user: BOT_USER,
+    };
+
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, [msg]),
+    }));
+  }; */
+
   return (
     <View style={styles.container}>
       <GiftedChat
