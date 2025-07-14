d61779f279153a2d6298df00917db8d05c7fb872
rob-cost <robertocostantino3@gmail.com>
2025-07-14 18:14:23 +0200

    Implemented local storage saving data
    Imported async-storage and netinfo libraries in the project. Set-up messages to be displayed in case the system is offline.

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Alert } from "react-native";
 import Start from "./components/Start";
 import Chat from "./components/Chat";
 import { NavigationContainer } from "@react-navigation/native";
+import { useEffect } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import {
+  getFirestore,
+  disableNetwork,
+  enableNetwork,
+} from "firebase/firestore";
 import ChatBot from "./components/ChatBot";
+import { useNetInfo } from "@react-native-community/netinfo";
 
 const App = () => {
   const Stack = createNativeStackNavigator();
@@ -22,13 +28,30 @@ const App = () => {
 
   const app = initializeApp(firebaseConfig);
   const db = getFirestore(app);
+  const connectionStatus = useNetInfo(); // Returns boolean value depending from the connection state
+
+  // Check tthe connection status
+  useEffect(() => {
+    if (connectionStatus.isConnected === false) {
+      Alert.alert("Connection lost");
+      disableNetwork(db);
+    } else if (connectionStatus.isConnected === true) {
+      enableNetwork(db);
+    }
+  }, [connectionStatus.isConnected]);
 
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Start">
         <Stack.Screen name="Start" component={Start} />
         <Stack.Screen name="Chat">
-          {(props) => <Chat db={db} {...props} />}
+          {props => (
+            <Chat
+              isConnected={connectionStatus.isConnected}
+              db={db}
+              {...props}
+            />
+          )}
         </Stack.Screen>
         <Stack.Screen name="ChatBot" component={ChatBot} />
       </Stack.Navigator>
--- components/Chat.js
+++ components/Chat.js
@@ -15,48 +15,84 @@ import {
   TouchableOpacity,
   Alert,
 } from "react-native";
-import { Bubble, GiftedChat } from "react-native-gifted-chat";
+import { Bubble, GiftedChat, InputToolbar } from "react-native-gifted-chat";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const Chat = ({ route, navigation, db }) => {
-  const { name, color, userID } = route.params; // REN: Received name, userID and color from Start.js while navigating
+const Chat = ({ route, navigation, db, isConnected }) => {
+  const { name, color, userID } = route.params; // Received name, userID and color from Start.js while navigating
   const [messages, setMessages] = useState([]); // Create state for message
+  let unSubscribeMsgs;
 
   useEffect(() => {
+    // Using navigation.setOptions we assing the name as title of the bar header
     navigation.setOptions({ title: name });
 
-    // Ren: Using firestore onSnapshot, we query messages from firebase and set it to messages state.
-    // https://firebase.google.com/docs/firestore/query-data/listen
-    let unSubscribeMsgs;
-    const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
-    unSubscribeMsgs = onSnapshot(q, (querySnapshot) => {
-      let newMsgs = [];
-      console.log("TEST: Fetch All Messages", querySnapshot);
-      querySnapshot.forEach((doc) => {
-        newMsgs.push({
-          ...doc.data(), // REN: DELETE all the docs in the firebase, then enable this line and delete above 7 lines.
-          createdAt: new Date(doc.data().createdAt.toMillis()),
+    // Using firestore onSnapshot, we query messages from firebase and set it to messages state.
+    if (isConnected === true) {
+      const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+      unSubscribeMsgs = onSnapshot(q, querySnapshot => {
+        let newMsgs = [];
+        console.log("TEST: Fetch All Messages", querySnapshot);
+        querySnapshot.forEach(doc => {
+          newMsgs.push({
+            ...doc.data(),
+            createdAt: new Date(doc.data().createdAt.toMillis()),
+          });
         });
+
+        // We set the value of messages with the value fetched from the db
+        cacheMessages(newMsgs);
+        setMessages(newMsgs);
+        console.log(isConnected);
+        console.log(cacheMessages);
       });
-      setMessages(newMsgs);
-    });
+    } else {
+      loadCacheMessages();
+    }
 
     // Ren: Clean up code
     return () => {
       if (unSubscribeMsgs) {
         unSubscribeMsgs();
+        unSubscribeMsgs = null;
       }
     };
-  }, []);
+  }, [isConnected]);
+
+  // We cache the messages using async storage function after they are fetched from the db
+  const cacheMessages = async msgsToCache => {
+    try {
+      await AsyncStorage.setItem("messages", JSON.stringify(msgsToCache));
+    } catch (err) {
+      console.log("Unable to cache messages " + err.message);
+    }
+  };
+
+  // We load the messages from the cache in case of no connection
+  const loadCacheMessages = async () => {
+    try {
+      const cachedMsgs = await AsyncStorage.getItem("messages");
+      if (cachedMsgs) {
+        setMessages(JSON.parse(cachedMsgs));
+      } else {
+        setMessages([]);
+      }
+    } catch (err) {
+      console.log("Unable to load cache messages");
+      setMessages([]);
+    }
+    console.log(messages);
+  };
 
   // sending message function
-  const onSend = (newMsg) => {
-    // Ren: We push the message to the Firebase using addDoc method
+  const onSend = newMsg => {
+    // We push the message to the Firebase using addDoc method
     console.log("TEST: Send Message", newMsg[0]);
     addDoc(collection(db, "messages"), newMsg[0]);
   };
 
   // customise message bubble
-  const renderBubble = (props) => {
+  const renderBubble = props => {
     return (
       <Bubble
         {...props}
@@ -72,6 +108,12 @@ const Chat = ({ route, navigation, db }) => {
     );
   };
 
+  // override input toolbar if connection is lost
+  const renderInputToolbar = props => {
+    if (isConnected) return <InputToolbar {...props} />;
+    else return null;
+  };
+
   return (
     <View style={[styles.container, { backgroundColor: color }]}>
       <View>
@@ -85,8 +127,9 @@ const Chat = ({ route, navigation, db }) => {
       </View>
       <GiftedChat
         messages={messages}
-        onSend={(messages) => onSend(messages)}
+        onSend={messages => onSend(messages)}
         renderBubble={renderBubble}
+        renderInputToolbar={renderInputToolbar}
         user={{
           _id: userID,
           name: name || "Anonymous",
